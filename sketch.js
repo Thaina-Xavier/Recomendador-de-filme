196068e8f422a2899b19e5aedefba98498a3dfdb
Thainá Xavier <thaina.xavier.araujo@escola.pr.gov.br>
2024-09-27 08:18:18 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,97 @@
+SKIP TO PLAY SKETCH
+Cookies
+The p5.js Editor uses cookies. Some are essential to the website functionality and allow you to manage an account and preferences. Others are not essential—they are used for analytics and allow us to learn more about our community. We never sell this data or use it for advertising. You can decide which cookies you would like to allow, and learn more in our Privacy Policy.
+
+
+Allow All
+
+Allow Essential
+File
+Edit
+Sketch
+Help
+English
+Hello, Thaina_X!
+
+
+
+Auto-refresh
+
+Recomendador de filmes - aula 8(final) 3ª série
+by alexandreborgonovi
+
+
+Sketch Files
+
+i
+nde
+x
+.html
+
+s
+ketc
+h
+.js
+
+s
+tyl
+e
+.css
+
+sketch.js
+31
+    } else {
+32
+      if (idade >= 12) {
+33
+        if(gostaDeFantasia || gostaDeAventura) {
+34
+          return "Homem aranha: no aranhaverso";          
+35
+        } else{
+36
+         return "Ladrões de bicicleta";
+37
+        }
+38
+      } else {
+39
+        if (gostaDeFantasia) {
+40
+          return "As aventuras de pi";
+41
+        } else {
+42
+          return "Depois da chuva";
+43
+        }
+44
+      }
+45
+    }
+46
+  } else {
+47
+    if (gostaDeFantasia) {
+48
+      return "A viagem de chihiro";
+49
+    } else {
+50
+      return "O feitiço do tempo";
+51
+    }
+52
+  }
+53
+}
+54
+​
+There are no lint messages
+Current lineline 54
+
+Console
+Clear
+
+​
+Preview
